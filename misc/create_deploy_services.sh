440ea19ed49b0b81b2bf9910a05c6aa699c46f40
Mike Bright <openshift@mjbright.net>
2018-04-25 19:00:11 +0200

    quick create/delete scripts for service/deploys

---
+++ misc/create_deploy_services.sh
@@ -0,0 +1,14 @@
+
+SCRIPT_DIR=$(dirname $0)
+
+set -x
+kubectl apply -f redis-deployment.yaml 
+kubectl apply -f flask-deployment.yaml 
+kubectl apply -f redis-service.yaml 
+kubectl apply -f flask-service.yaml 
+
+kubectl get svc
+set +x
+
+$SCRIPT_DIR/get_minikube_service_url.sh
+
---
+++ misc/delete_deploy_services.sh
@@ -0,0 +1,7 @@
+
+kubectl delete -f redis-deployment.yaml 
+kubectl delete -f flask-deployment.yaml 
+kubectl delete -f redis-service.yaml 
+kubectl delete -f flask-service.yaml 
+
+
