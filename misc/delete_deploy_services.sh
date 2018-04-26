3bf32021f81cf061cba6d6f5a4a1d004d6d29cf0
Mike Bright <openshift@mjbright.net>
2018-04-26 09:59:49 +0200

    Added yaml resources for k8sdemo

--- misc/delete_deploy_services.sh
+++ misc/delete_deploy_services.sh
@@ -4,4 +4,7 @@ kubectl delete -f flask-deployment.yaml
 kubectl delete -f redis-service.yaml 
 kubectl delete -f flask-service.yaml 
 
+kubectl delete -f k8sdemo-service.yaml 
+kubectl delete -f k8sdemo-service.yaml 
+
 
